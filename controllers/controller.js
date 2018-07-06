efbdd08bc7a9f4f7cb07683a89153dc44600e5de
aricci <alexander.p.ricci@gmail.com>
2018-07-06 15:17:18 -0700

    made changes to all files, got id of some bugs, still need to work on notes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,129 +1,107 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
-var db = require("./models");
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-var exphbs = require('express-handlebars');
 var router = express.Router();
 var request = require("request");
-
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
+var cheerio = require("cheerio");
+var mongoose = require("mongoose");
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
 
+var Note = require("../models/Note.js");
+var Article = require("../models/Article.js");
 
-
-app.get("/", function (req, res) {
-    res.send("index");
+router.get("/", function(req, res) {
+  res.render("index");
 });
 
-
-app.get("/save", function(req, res) {
-    Article.find({})
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-    res.render("save");
-});
-
-
-app.get("/scrape", function(req, res) {
-    axios.get("http://www.echojs.com/").then(function(response) {
-      var $ = cheerio.load(response.data);
-  
-      $("article h2").each(function(i, element) {
-            var result = {};
-    
-            result.title = $(this).children("a").text();
-            result.link = $(this).children("a").attr("href");
-    
-            db.Article.create(result)
-            .then(function(dbArticle) {
-                console.log(dbArticle);
-            })
-            .catch(function(err) {
-                return res.json(err);
-            });
-      });
-        res.redirect("index");
-    });
-});
-  
-//EDIT
-app.post("/save", function(req, res) {
-    var addArticle = {};
-    addArticle.title = req.body.title;
-    addArticle.link = req.body.link;
-    var entry = new Article(addArticle);
-    entry.save(function(err, doc) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log(doc);
-        }
-    });
-    res.redirect("/save");
-});
-
-
-app.get("/delete/:id", function(req, res) {
-    Article.findOneAndRemove({"_id": req.params.id})
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
-    res.redirect("/save");
-});
-
-
-app.get("/notes/:id", function(req, res) {
-    Note.findOneAndRemove({"_id": req.params.id})
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
-    });
-    res.send(dbArticle);
+router.get("/save", function(req, res) {
+  Article.find({}, function(error, doc) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      var ArticleObject = {
+        articles: doc
+      };
+      res.render("save", ArticleObject);
+    }
   });
+});
 
 
-app.get("/articles/:id", function(req, res) {
-    Article.findOne({"_id": req.params.id})
-    .populate('notes')
-    .then(function(dbArticle) {
-        res.json(dbArticle);
-    })
-    .catch(function(err) {
-        res.json(err);
+router.post("/scrape", function(req, res) {
+  request("http://www.nytimes.com/", function(error, response, html) {
+    var $ = cheerio.load(html);
+    $("article h2").each(function(i, element) {
+      var result = {};
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
     });
+    res.render("index");
+  });
 });
-  
-  
-app.post("/articles/:id", function(req, res) {
-    db.Note.create(req.body)
-    .then(function(dbNote) {
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        res.json(err);
+
+
+router.post("/save", function(req, res) {
+  var newArticleObject = {};
+  newArticleObject.title = req.body.title;
+  newArticleObject.link = req.body.link;
+  var entry = new Article(newArticleObject);
+  entry.save(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(doc);
+    }
+  });
+  res.redirect("/save");
+});
+
+
+router.get("/delete/:id", function(req, res) {
+  Article.findOneAndRemove({"_id": req.params.id}, function (err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("OK");
+    }
+    res.send(doc);
+    res.redirect("/save");
+  });
+});
+
+
+router.get("/articles/:id", function(req, res) {
+  Article.findOne({"_id": req.params.id})
+  .populate('notes')
+  .exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+
+router.post("/articles/:id", function(req, res) {
+  var newNote = new Note(req.body);
+  newNote.save(function(err, doc) {
+    if (error) {
+      console.log(err);
+    }
+    else {
+      Article.findOneAndUpdate({ "_id": req.params.id }, {$push: {notes: doc._id}}, {new: true})
+      .populate('notes')
+      .exec(function (err, doc) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(doc);
+        }
       });
+    }
+  });
 });
 
-
-  module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
-var db = require("./models");
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-var exphbs = require('express-handlebars');
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -22,6 +13,7 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
@@ -31,5 +23,5 @@ app.use("/", routes);
 mongoose.connect("mongodb://heroku_k6rk8rtl:2mfqgi5u4mbl6jg8bovgr0emcv@ds127781.mlab.com:27781/heroku_k6rk8rtl");
 
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+  console.log("App running on PORT " + PORT);
 });
