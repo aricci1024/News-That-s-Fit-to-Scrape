b44deefd840b432f0d38ad84a749d8de0aab1fd6
aricci <alexander.p.ricci@gmail.com>
2018-07-05 19:49:40 -0700

    updating routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,49 +8,85 @@ var db = require("./models");
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
 var exphbs = require('express-handlebars');
+var router = express.Router();
+var request = require("request");
 
-// Routes
 
-// A GET route for scraping the echoJS website
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+
+app.get("/", function (req, res) {
+    res.send("index");
+});
+
+
+app.get("/save", function(req, res) {
+    Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+    res.render("save");
+});
+
+
 app.get("/scrape", function(req, res) {
-    // First, we grab the body of the html with request
     axios.get("http://www.echojs.com/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
-      // Now, we grab every h2 within an article tag, and do the following:
       $("article h2").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-  
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-  
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-          });
+            var result = {};
+    
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
+    
+                db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    return res.json(err);
+                });
       });
-  
-      // If we were able to successfully scrape and save an Article, send a message to the client
-      res.redirect("/");
+        res.redirect("index");
     });
   });
   
-  app.get("/", function (req, res) {
-    res.send(index.html);
+
+app.post("/save", function(req, res) {
+    var addArticle = {};
+    addArticle.title = req.body.title;
+    addArticle.link = req.body.link;
+    var entry = new Article(addArticle);
+    entry.save(function(err, doc) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(doc);
+        }
+    });
+    res.redirect("/save");
   });
+
+
+app.get("/delete/:id", function(req, res) {
+    Article.findOneAndRemove({"_id": req.params.id}, function (err, offer) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Ok");
+        }
+    });
+    res.redirect("/save");
+  });
+
   
   // Route for getting all Articles from the db
   app.get("/articles", function(req, res) {
